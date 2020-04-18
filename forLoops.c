3b790e7115896bc32ac7d705ab9e6a9ddfb040c0
Vitor Emanuel <veslima3@gmail.com>
2020-04-18 00:09:00 -0300

    Update forLoops.c

--- forLoops.c
+++ forLoops.c
@@ -13,63 +13,63 @@ int main()
 
     if (a >= 1 && b >= a && b <= pow(10,6)){
 
-  	for(int n = 1; n <= 9; n++) {
+  	    for(int n = a; n <= b; n++) {
         
-        if (n == 1){
+            if (n == 1){
 
-            printf("one");
+                printf("one\n");
 
-        } else if(n == 2){
+            } else if(n == 2){
 
-            printf("two");
+                printf("two\n");
 
-        }else if(n == 3){
+            }else if(n == 3){
 
-            printf("three");
+                printf("three\n");
         
-        }else if(n == 4){
+            }else if(n == 4){
 
-            printf("four");
+                printf("four\n");
 
-        }else if(n == 5){
+            }else if(n == 5){
 
-            printf("five");
+                printf("five\n");
 
-        }else if(n == 6){
+            }else if(n == 6){
 
-            printf("six");
+                printf("six\n");
         
-        }else if(n == 7){
+            }else if(n == 7){
 
-            printf("seven");
+                printf("seven\n");
 
-        }else if(n == 8){
+            }else if(n == 8){
 
-            printf("eight");
+                printf("eight\n");
         
-        }else if(n == 9){
+            }else if(n == 9){
 
-            printf("nine");
+                printf("nine\n");
+
+            } else if (n > 9){
+
+                if(n % 2 != 0){
+
+                    printf("odd\n");
+
+                }else if(n % 2 == 0){
+
+                    printf("even\n");    
+
+                }
+
+            }
 
         }
     
     }
 
-    for(int j = b + 1; j >= b; j--) {
-        
-        if(j % 2 != 0){
 
-            printf("\nodd");
-
-        }else if (j % 2 == 0){
-
-            printf("\neven");
-
-        }
-
-    }
-
-    }
 
     return 0;
 }
